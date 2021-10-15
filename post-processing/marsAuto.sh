105ec12fe26196147bb40435e60dcf61ec539f24
Ignacio <isampedro@itba.edu.ar>
2021-10-14 23:44:59 -0300

    Fixed mars bash

--- post-processing/marsAuto.sh
+++ post-processing/marsAuto.sh
@@ -3,7 +3,7 @@ cd ..
 echo -e "v\tt\td" > output/marsLanding.txt
 for V in $(seq 7.999 0.00001 8.002 )
 	do 
-		echo -e "4\n100\n${V}" > input.txt
+		echo -e "5\n100\n${V}" > input.txt
 		java -jar bin/SS-TP4-1.0-SNAPSHOT-jar-with-dependencies.jar >> output/marsLanding.txt
 	done
 cd post-processing
